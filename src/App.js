b360cb05a4a53250a8ef04e1bc48ac1346fbaa50
Cody Brunner <rockchalkwushock@icloud.com>
2017-10-04 13:06:28 -0500

    feat(src): Added button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button>
+          <a href="https://youtu.be/L59eDcrAk8Q">Go</a>
+        </button>
       </div>
     )
   }
